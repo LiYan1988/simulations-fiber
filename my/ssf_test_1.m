c956c730cc84e44de13a8040be3e3b80fbe5ee76
LiYan1988 <lil.yan.ly414@gmail.com>
2018-08-07 13:37:23 +0200

    Test a simple example of SSF

---
+++ my/ssf_test_1.m
@@ -0,0 +1,108 @@
+clc;
+clear;
+close all;
+
+% --- Frequency parameters
+% 400 GHz frequency span, frequency axis is [-fmax:fmax-df]/(2*pi)
+% fmax is the maximum frequency on one side, the frequency window is 2*fmax
+fmax = 2*pi*10; % [Hz]
+% number of spectrum points
+fn = 2^10;
+% spectrum resolution
+df = 2*fmax/fn; % [Hz]
+% spectrum axis
+% f is used for calculation, for visualization of spectrum use f/(2*pi)
+f = df*[(0:fn/2-1) (-fn/2:-1)];
+% ---
+
+% --- Time parameters
+% Time domain
+% time window size is 2*pi/df, time axis is [-tmax:tmax-dt]
+tmax = pi/df; % [s]
+% time resolution
+dt = 2*tmax/fn; % [s]
+% time axis
+t = dt*(-fn/2:fn/2-1);
+% ---
+
+% --- Fiber parameters
+% D = 17 [ps/nm/km]
+% wavelength = 1.55 [um]
+% speed_light = 2.99792458e5 [km/s]
+% beta2 = -D*wavelength^2/(speed_light*2*pi) [ns^2/km]
+distance = 10; % [km], span length
+beta2 = -2.1683e-05; % [ns^2/km], GVD
+gamma = 1.27e-3; % [(W*m)^-1], nonlinear coefficient of SMF 
+alpha = 0.2; % [dB/km], attenuation in dB
+alpha = log(10)*alpha/10; % [1/km] in linear
+% ---
+
+% --- Spatial parameters
+Ld = dt^2/abs(beta2)*1e18; % [km], dispersion distance
+% zn = round(20*distance/Ld/gamma^2); % Number of z steps
+zn = 1000;
+dz = distance/zn; % [km], step size in z
+% ---
+
+% --- Generate a pulse
+uu = sech(t).*exp(-0.5*1i*1*t.^2);
+% ---
+
+% plot(t, abs(uu).^2)
+
+% --- Plot input pulse shape and spectrum
+temp = fftshift(ifft(uu)).*(fn*dt)/sqrt(2*pi); % spectrum
+figure; 
+hold on;
+subplot(2,1,1);
+h1 = plot(t, abs(uu).^2, '-b', 'linewidth', 2, 'displayname', 'Input'); 
+hold on;
+axis([-tmax tmax 0 max(abs(uu).^2)*2]);
+xlabel('Time');
+ylabel('Power');
+title('Input and Output Pulse Shape and Spectrum');
+subplot(2,1,2);
+h2 = plot(fftshift(f)/(2*pi), abs(temp).^2, 'ob', 'linewidth', 2, 'displayname', 'Input');  
+hold on;
+axis([-fmax fmax 0 max(abs(temp).^2)*2]);
+xlabel('Frequency');
+ylabel('Spectral Power');
+% ---
+
+% --- Store dispersive phase shifts to speedup code
+dispersion = exp(0.5*1i*beta2*f.^2*dz); % phase factor
+hhz = 1i*gamma^2*dz; % nonlinear phase factor
+% ---
+
+% --- Plot phase
+% N = 1e1;
+% plot(unwrap(angle(dispersion(1:N))), 'linewidth', 2, 'linestyle', '--')
+% hold on;
+% plot(0.5*beta2*f(1:N).^2*dz, 'linewidth', 2)
+% ---
+
+% --- Main loop
+% scheme: 1/2N -> D -> 1/2N; first half step nonlinear
+temp = uu.*exp(abs(uu).^2.*hhz/2); % note hhz/2
+for n=1:zn
+    f_temp = ifft(temp).*dispersion;
+    uu = fft(f_temp);
+    temp = uu.*exp(abs(uu).^2.*hhz);
+end
+uu = temp.*exp(-abs(uu).^2.*hhz/2); % Final field
+temp = fftshift(ifft(uu)).* (fn*dt)/sqrt(2*pi); % Final spectrum
+% ---
+
+% ----Plot output pulse shape and spectrum
+subplot(2,1,1)
+h3 = plot (t, abs(uu).^2, '--r', 'linewidth', 2, 'displayname', 'Output'); 
+axis([-tmax tmax 0 max(abs(uu).^2)*2]);
+legend([h1, h3])
+subplot(2,1,2)
+h4 = plot(fftshift(f)/(2*pi), abs(temp).^2, '--r', 'linewidth', 2, 'displayname', 'Output'); 
+axis([-fmax fmax 0 max(abs(temp).^2)*2]);
+legend([h2, h4])
+
+% In this example, the bandwidth of the pulse is 1 Hz. The frequency
+% resolution is df/(2*pi)=0.0195 Hz. The frequency oversampling rate is
+% around 50.
