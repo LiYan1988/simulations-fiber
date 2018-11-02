917c9c4a7e1e50e4f0c32706a218ebdd6316ddff
LiYan1988 <li.yan.ly414@gmail.com>
2018-11-02 21:42:20 +0100

    generate signal

--- single-polarization/SinglePolarization.m
+++ single-polarization/SinglePolarization.m
@@ -1,4 +1,4 @@
-classdef SinglePolarization
+classdef SinglePolarization < handle
     %Single polarization simulation of hybrid channels
     %   Detailed explanation goes here
     
@@ -50,6 +50,15 @@ classdef SinglePolarization
         dt
         % time axis [s], a column vector
         t
+        
+        % Transmitted signal in time domain
+        txSignalTime
+        % Transmitted signal in spectrum domain
+        txSignalSpectrum
+        % Current signal in time domain
+        currentSignalTime
+        % Current Signal in spectrum domain
+        currentSignalSpectrum
     end
     
     %% Dependent parameters
@@ -137,9 +146,17 @@ classdef SinglePolarization
             obj.t = (-obj.N/2:obj.N/2-1).'*obj.dt;
             
             %% Generate signals
+            % Transmitted signal in time domain
+            obj.txSignalTime = zeros(obj.N, 1);
             for n=1:obj.numberChannel
-                generateSignal(obj.channelArray(n));
+                generateSignal(obj.channelArray(n), obj);
             end
+            % Transmitted signal in spectrum domain
+            obj.txSignalSpectrum = ft(obj.txSignalTime, obj.dt);
+            
+            % Current signal in time and spectrum domains
+            obj.currentSignalTime = obj.txSignalTime;
+            obj.currentSignalSpectrum = obj.txSignalSpectrum;
         end
         
         function numberLink = get.numberLink(obj)
@@ -239,6 +256,11 @@ function generateOOK(channel)
 
 % Generate bit stream
 channel.dataBit = randi([0, 1], channel.actualNumberSymbol, channel.bitPerSymbol);
+% The first and last ceil(channel.symbolInFir/2) symbols are affected by
+% FIR convolution effects. Set them to 0 to eliminate this effect.
+halfFirSymbolLength = ceil(channel.symbolInFir/2);
+channel.dataBit(1:halfFirSymbolLength) = 0;
+channel.dataBit(end-halfFirSymbolLength+1:end) = 0;
 
 % Symbol stream
 channel.dataSymbol = channel.dataBit;
@@ -250,8 +272,15 @@ dataTimeLength = length(channel.dataTime);
 channel.fir = gaussdesign(channel.firFactor, channel.symbolInFir, channel.actualSamplePerSymbol);
 % Pass dataTime through FIR
 channel.dataTime = upfirdn(channel.dataTime, channel.fir);
+
+% Shift signal randomly within one symbol time
+channel.shiftNumberSample = randi([0, channel.actualSamplePerSymbol-1]);
+% channel.dataTime = [zeros(channel.shiftNumberSample, 1); ...
+%     channel.dataTime(1:end-channel.shiftNumberSample)];
+channel.dataTime = circshift(channel.dataTime, channel.shiftNumberSample);
+
 % FIR delay in number of samples
-firOverhead = (length(channel.fir)-1)/2;
+firOverhead = ceil((length(channel.fir)-1)/2);
 % Remove head and tail added by FIR and convolution inside upfirdn
 s = (firOverhead+1):(firOverhead+dataTimeLength);
 channel.dataTime = channel.dataTime(s);
@@ -260,6 +289,11 @@ end
 function generate16QAM(channel)
 % Generate 16QAM bits
 channel.dataBit = randi([0, 1], channel.actualNumberSymbol, channel.bitPerSymbol);
+% The first and last ceil(channel.symbolInFir/2) symbols are affected by
+% FIR convolution effects. Set them to 0 to eliminate this effect.
+halfFirSymbolLength = ceil(channel.symbolInFir/2);
+channel.dataBit(1:halfFirSymbolLength, :) = 0;
+channel.dataBit(end-halfFirSymbolLength+1:end, :) = 0;
 
 % Symbols
 channel.dataSymbol = bi2de(channel.dataBit);
@@ -272,20 +306,48 @@ channel.fir = rcosdesign(channel.firFactor, channel.symbolInFir, channel.actualS
 dataTimeLength = channel.actualSamplePerSymbol*channel.actualNumberSymbol;
 % Pass through FIR with upsample
 channel.dataTime = upfirdn(channel.dataSymbol, channel.fir, channel.actualSamplePerSymbol);
+
+% Shift signal randomly within one symbol time
+channel.shiftNumberSample = randi([0, channel.actualSamplePerSymbol-1]);
+% channel.dataTime = [zeros(channel.shiftNumberSample, 1); ...
+%     channel.dataTime(1:end-channel.shiftNumberSample)];
+channel.dataTime = circshift(channel.dataTime, channel.shiftNumberSample);
+
 % Remove head and tail of dataTime
 % This is the head to be removed, because the output length of FIR is
 % ceil(((length(xin)-1)*p+length(h))/q) for yout = upfirdn(xin,h,p,q)
 % See Matlab reference of upfirdn and conv
-firOverhead = floor((length(channel.fir)-channel.actualSamplePerSymbol)/2);
+firOverhead = ceil((length(channel.fir)-channel.actualSamplePerSymbol)/2);
 s = (firOverhead+1):(firOverhead+dataTimeLength);
 channel.dataTime = channel.dataTime(s);
 end
 
-function generateSignal(channel)
+function generateSignal(channel, obj)
 % Generate signal according to modulation format
 if strcmp(channel.modulation, 'OOK')
     generateOOK(channel);
 elseif strcmp(channel.modulation, '16QAM')
     generate16QAM(channel);
 end
+
+% Assign power to the signal
+powerNormalized = norm(channel.dataTime)^2/obj.N;
+channel.dataTime = channel.dataTime*sqrt(channel.powerW/powerNormalized);
+
+% Move in spectrum to its center frequency
+channel.dataTime = channel.dataTime.*...
+    exp(-1i*2*pi*channel.centerFrequency.*obj.t);
+
+% Add channel signal to the total transmitted signal
+obj.txSignalTime = obj.txSignalTime+channel.dataTime;
+end
+
+function xf = ft(xt, df)
+% Fourier transform
+xf = fftshift(ifft(fftshift(xt)))/sqrt(df/(2*pi));
+end
+
+function xt = ift(xf, df)
+% Inverse Fourier transform
+xt = fftshift(fft(fftshift(xf)))*sqrt(df/(2*pi));
 end
--- single-polarization/testClass.m
+++ single-polarization/testClass.m
@@ -4,20 +4,20 @@ clear;
 close all;
 
 %% Default properties
-channelArray = Channel();
+channelArray = Channel('centerFrequency', -50e9);
 linkArray = Link();
 
 %% Other property values
 channelArray(2) = Channel('modulation', '16QAM', 'symbolRate', 32e9, ...
-    'centerFrequency', 50e9, 'firFactor', 0.2, ...
-    'minSamplePerSymbol', 16);
+    'centerFrequency', 0e9, 'firFactor', 0.2, ...
+    'minSamplePerSymbol', 16, 'minNumberSymbol', 2^14);
 
 linkArray(2) = Link('spanLength', 80e3, 'D', 1.6e-5, 'S', 0, ...
     'alphadB', 0.25e-3, 'gamma', 1.5e-3, 'NFdB', 6, 'DCFLength', 80e3, ...
     'numberSteps', 200);
 
 %% Partial default values
-channelArray(3) = Channel('centerFrequency', -50e9);
+channelArray(3) = Channel('centerFrequency', 50e9);
 
 linkArray(3) = Link('DCFLength', 90e3);
 
@@ -25,7 +25,8 @@ linkArray(3) = Link('DCFLength', 90e3);
 % link(4) = Link('modulation', '32QAM');
 
 %% SinglePolarization
-sp = SinglePolarization('linkArray', linkArray, 'channelArray', channelArray);
+sp = SinglePolarization('linkArray', linkArray, ...
+    'channelArray', channelArray, 'randomSeed', 2938);
 fclose(sp.logFid);
 clearvars linkArray channelArray
 
@@ -37,8 +38,12 @@ domega = sp.domega;
 N = sp.N;
 
 %% Plot dataTime 
-plot(t, abs(sp.channelArray(2).dataTime))
-figure;
-plot(t, abs(sp.channelArray(1).dataTime))
-figure;
-plot(t, abs(sp.channelArray(3).dataTime))
+% plot(t, abs(sp.channelArray(2).dataTime))
+% figure;
+% plot(t, abs(sp.channelArray(1).dataTime))
+% figure;
+% plot(t, abs(sp.channelArray(3).dataTime))
+plot(t, abs(sp.txSignalTime))
+
+%% Plot Spectrum domain signal
+plot(omega/2/pi, 10*log10(abs(sp.txSignalSpectrum).^2*1e12))
