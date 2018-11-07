5261b115fa4581c94529179740379443efa8aa1f
LiYan1988 <li.yan.ly414@gmail.com>
2018-11-07 13:48:22 +0100

    test single job on Glenn

--- single-polarization/cluster/testCluster.sh
+++ single-polarization/cluster/testCluster.sh
@@ -1,18 +1,24 @@
 #!/bin/bash 
-#SBATCH -n 1  
+#SBATCH -N 1 -n 1  
 
 #SBATCH -p glenn 
-#SBATCH -A C3SE407-15-3
+#SBATCH -A C3SE2018-1-15
 #SBATCH -o testCluster.stdout
-#SBATCH -o testCluster.stderr
+#SBATCH -e testCluster.stderr
 #SBATCH -t 0-00:10:00 
+
+echo $TMPDIR
+echo $SLURM_SUBMIT_DIR
 
 cp -pr * $TMPDIR/
 cd $TMPDIR
 
-module load MATLAB
-matlab -nodesktop -nosplash -singleCompThread -r "testCluster.m"
+module load matlab
+matlab -nodesktop -nosplash -singleCompThread -r "testCluster"
 
 wait
 
 cp -pr * $SLURM_SUBMIT_DIR/ 
+
+echo $TMPDIR
+echo $SLURM_SUBMIT_DIR
