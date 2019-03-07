23f82519744cf53e64825ebe1f8c8a8512771d6f
LiYan1988 <li.yan.ly414@gmail.com>
2019-03-07 15:04:23 +0100

    plot figures for interleave 3 OOK 200 GHz

--- single-polarization/simulationForChayan-3/interleave3OOK/postProcess_powerOOK_0dBm_channelSpacing_200GHz.m
+++ single-polarization/simulationForChayan-3/interleave3OOK/postProcess_powerOOK_0dBm_channelSpacing_200GHz.m
@@ -41,16 +41,18 @@ QAMSNRdB = mean(resultLevel1.SNRdB(:, [2, 4]), 2);
 QAMSymbolRate = resultLevel1.symbolRate(:, 2);
 QAMPowerdBm = resultLevel1.powerdBm(:, 2);
 OOKSNRdB = min(resultLevel1.SNRdB(:, [1, 3, 5]), [], 2);
-totalAchievableRate = sum(resultLevel1.achievableDataRate(:, [2, 4]), 2)/1e9;
+qamAIR = mean(resultLevel1.achievableDataRate(:, [2, 4]), 2)/1e9;
+ookAIR = mean(resultLevel1.achievableDataRate(:, [1, 3, 5]), 2)/1e9;
 
-results = [QAMSymbolRate, QAMPowerdBm, QAMSNRdB, OOKSNRdB, totalAchievableRate];
+results = [QAMSymbolRate, QAMPowerdBm, QAMSNRdB, OOKSNRdB, qamAIR, ookAIR];
 results = sortrows(results, [1, 2]);
 
 meshYpower = reshape(results(:, 2), 31, []);
 meshXsym = reshape(results(:, 1), 31, [])*1e-9;
 meshQAM = reshape(results(:, 3), 31, []);
 meshOOK = reshape(results(:, 4), 31, []);
-meshAIR = reshape(results(:, 5), 31, []);
+meshQAMAIR = reshape(results(:, 5), 31, []);
+meshOOKAIR = reshape(results(:, 6), 31, []);
 
 %%
 figureFolder = sprintf('figures_%dGHz', channelSpacing);
@@ -60,7 +62,7 @@ end
 
 %%
 fig = figure;
-[M, c] = contour(meshXsym, meshYpower, meshQAM, [13, 12, 11, 10]);
+[M, c] = contour(meshXsym, meshYpower, meshQAM, [16, 15, 14, 13, 12, 11, 10]);
 c.LineWidth = 2;
 c.ShowText  = 'off';
 clabel(M,c,'Interpreter', 'latex', 'LabelSpacing', 360)
@@ -74,7 +76,7 @@ fig.PaperPosition = [0 0 8 4.5];
 colormap(parula)
 % c = colorbar;
 % c.Label.String = 'SNR (dB)';
-caxis([9, 14]) % [min-(max-min)/3, max+(max-min)/3]
+caxis([9, 17]) % [min-(max-min)/3, max+(max-min)/3]
 
 savefig(fullfile(figureFolder, sprintf('QAMSNR_%dGHz.fig', channelSpacing)))
 print(fullfile(figureFolder, sprintf('QAMSNR_%dGHz', channelSpacing)), '-dpng', '-r600')
@@ -107,7 +109,7 @@ OOKSNRth = 18;
 fig = figure;
 meshTemp = meshQAM;
 meshTemp(meshOOK<OOKSNRth) = 0;
-[M, c] = contour(meshXsym, meshYpower, meshTemp, [13, 12, 11, 10]);
+[M, c] = contour(meshXsym, meshYpower, meshTemp, [16, 15, 14, 13, 12, 11, 10]);
 c.LineWidth = 2;
 c.ShowText  = 'off';
 clabel(M,c,'Interpreter', 'latex', 'LabelSpacing', 600)
@@ -121,7 +123,7 @@ fig.PaperPosition = [0 0 8 4.5];
 colormap(parula)
 % c = colorbar;
 % c.Label.String = 'SNR (dB)';
-caxis([9, 14]) % [min-(max-min)/3, max+(max-min)/3]
+caxis([9, 17]) % [min-(max-min)/3, max+(max-min)/3]
 
 savefig(fullfile(figureFolder, sprintf('QAMSNR_OOKSNR_greater_than_%ddB_%dGHz.fig', OOKSNRth, channelSpacing)))
 print(fullfile(figureFolder, sprintf('QAMSNR_OOKSNR_greater_than_%ddB_%dGHz', OOKSNRth, channelSpacing) ), '-dpng', '-r600')
@@ -129,7 +131,7 @@ print(fullfile(figureFolder, sprintf('QAMSNR_OOKSNR_greater_than_%ddB_%dGHz', OO
 
 %%
 fig = figure;
-[M, c] = contour(meshXsym, meshYpower, meshAIR);
+[M, c] = contour(meshXsym, meshYpower, meshOOKAIR);
 c.LineWidth = 2;
 c.ShowText  = 'off';
 clabel(M,c,'Interpreter', 'latex', 'LabelSpacing', 600)
@@ -145,6 +147,31 @@ colormap(parula)
 % c.Label.String = 'SNR (dB)';
 % caxis([12, 27]) % [min-(max-min)/3, max+(max-min)/3]
 
-savefig(fullfile(figureFolder, sprintf('AIR_%dGHz.fig', channelSpacing)))
-print(fullfile(figureFolder, sprintf('AIR_%dGHz', channelSpacing)), '-dpng', '-r600')
-print(fullfile(figureFolder, sprintf('AIR_%dGHz', channelSpacing)), '-dpdf', '-r600', '-bestfit')
+savefig(fullfile(figureFolder, sprintf('OOKAIR_%dGHz.fig', channelSpacing)))
+print(fullfile(figureFolder, sprintf('OOKAIR_%dGHz', channelSpacing)), '-dpng', '-r600')
+print(fullfile(figureFolder, sprintf('OOKAIR_%dGHz', channelSpacing)), '-dpdf', '-r600', '-bestfit')
+
+%%
+OOKAIRth = 10;
+fig = figure;
+meshTemp = meshQAMAIR;
+meshTemp(meshOOKAIR<OOKAIRth) = 0;
+[M, c] = contour(meshXsym, meshYpower, meshTemp);
+c.LineWidth = 2;
+c.ShowText  = 'off';
+clabel(M,c,'Interpreter', 'latex', 'LabelSpacing', 600)
+set(gca, 'TickLabelInterpreter', 'latex')
+xlabel('QAM symbol rate (GBaud)', 'Interpreter', 'latex', 'fontsize', 12);
+ylabel('QAM power (dBm)', 'Interpreter', 'latex', 'fontsize', 12);
+grid on;
+pbaspect([8 4.5 1])
+fig.PaperPosition = [0 0 8 4.5];
+
+colormap(parula)
+% c = colorbar;
+% c.Label.String = 'SNR (dB)';
+% caxis([12, 27]) % [min-(max-min)/3, max+(max-min)/3]
+
+savefig(fullfile(figureFolder, sprintf('QAMAIR_OOKAIR_greater_than_%dGbps_%dGHz.fig', OOKAIRth, channelSpacing)))
+print(fullfile(figureFolder, sprintf('QAMAIR_OOKAIR_greater_than_%dGbps_%dGHz.fig', OOKAIRth, channelSpacing)), '-dpng', '-r600')
+print(fullfile(figureFolder, sprintf('QAMAIR_OOKAIR_greater_than_%dGbps_%dGHz.fig', OOKAIRth, channelSpacing)), '-dpdf', '-r600', '-bestfit')
